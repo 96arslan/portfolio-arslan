9f92497e475af01a20f63df3f6827298e8aa4942
Arslan <arslanmudasir111@gmail.com>
2023-09-23 21:09:27 +0530

    changed directories

--- arslan-portfolio/astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import { motion } from 'framer-motion';
-
-export default defineConfig({
-  integrations: [tailwind(),],
-});
-
