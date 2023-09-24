2a6a6e4648665cad7079515ec2c6f5265ad79fb9
Arslan <arslanmudasir111@gmail.com>
2023-09-24 16:17:30 +0530

    Added grid to bg, minor changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import { motion } from 'framer-motion';
 
 export default defineConfig({
   integrations: [tailwind(),],
